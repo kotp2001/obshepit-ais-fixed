482bbdf3e3f4089879c3fcec845dfea1ed0bdcda
kotp2001 <dubrovinkosta456@gmail.com>
2026-05-23 21:26:12 +0500

    Update models.py

--- restaurant/models.py
+++ restaurant/models.py
@@ -131,14 +131,9 @@ class Profile(models.Model):
         ('waiter', 'Официант'),
         ('chef', 'Повар'),
     ]
-    
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='waiter', verbose_name="Роль")
-    pin_code = models.CharField(max_length=4, blank=True, null=True, verbose_name="Пин-код")
-    
-    class Meta:
-        verbose_name = 'Профиль'
-        verbose_name_plural = 'Профили'
-    
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='waiter')
+    pin_code = models.CharField(max_length=4, blank=True, null=True)
+
     def __str__(self):
         return f"{self.user.username} - {self.get_role_display()}"
