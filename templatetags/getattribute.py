4ccc6c69bc5b420e8a8bad37ba3c458aea3d796b
kotp2001 <dubrovinkosta456@gmail.com>
2026-06-07 14:55:29 +0500

    Delete templatetags directory

--- templatetags/getattribute.py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter
-def getattribute(value, arg):
-    """Возвращает атрибут объекта по имени."""
-    return getattr(value, arg, '')
