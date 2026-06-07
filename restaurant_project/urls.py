e0fce4421716da14c626b53f3017010a32195872
kotp2001 <dubrovinkosta456@gmail.com>
2026-06-07 14:54:57 +0500

    Delete restaurant_project directory

--- restaurant_project/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('backup/', include('restaurant.backup_urls')),   # НЕ под admin/ !
-    path('', include('restaurant.urls')),
-]
