80216f61ebd6ad8b5c9e57c8b5dbb522e84529da
kotp2001 <dubrovinkosta456@gmail.com>
2026-06-07 14:54:48 +0500

    Delete restaurant directory

--- restaurant/templatetags/getattribute.py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter
-def getattribute(value, arg):
-    """Возвращает атрибут объекта по его имени."""
-    return getattr(value, arg, '')
