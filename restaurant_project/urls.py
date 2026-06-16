9724e33968abad2e5a37ac5a3bf4b464c14a0bf9
kotp2001 <dubrovinkosta456@gmail.com>
2026-06-16 20:25:05 +0500

    Update urls.py

--- restaurant_project/urls.py
+++ restaurant_project/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
+# Устанавливаем URL для админ-панели (вместо /admin/)
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('secret-admin/', admin.site.urls),  # <-- теперь админка доступна только по этому URL
     path('backup/', include('restaurant.backup_urls')),
     path('', include('restaurant.urls')),
 ]
