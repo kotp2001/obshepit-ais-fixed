53beec32269a2d1ac4e570f7686ba6ba3a73dc3c
kotp2001 <dubrovinkosta456@gmail.com>
2026-06-16 20:56:41 +0500

    Update backup_urls.py

--- restaurant/backup_urls.py
+++ restaurant/backup_urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from . import views
+from . import backup_views
 
 app_name = 'backup'
 
 urlpatterns = [
-    path('', views.backup_list, name='backup_list'),
-    path('create/', views.backup_create, name='backup_create'),
-    path('download/<str:filename>/', views.backup_download, name='backup_download'),
-    path('restore/<str:filename>/', views.backup_restore, name='backup_restore'),
-    path('delete/<str:filename>/', views.backup_delete, name='backup_delete'),
+    path('', backup_views.backup_list, name='backup_list'),
+    path('create/', backup_views.backup_create, name='backup_create'),
+    path('download/<str:filename>/', backup_views.backup_download, name='backup_download'),
+    path('restore/<str:filename>/', backup_views.backup_restore, name='backup_restore'),
+    path('delete/<str:filename>/', backup_views.backup_delete, name='backup_delete'),
+    path('upload/', backup_views.backup_upload, name='backup_upload'),
 ]
